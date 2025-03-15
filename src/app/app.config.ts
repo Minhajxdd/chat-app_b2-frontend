edfc1fbe3e0f25303cbd7d1d260e320b869e02d4
muhammedsirajudeen <muhammedsirajudeen29@gmail.com>
2025-03-15 09:33:31 +0530

    feat(app): registerd httpclient

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,9 +2,14 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { authRoutes } from './core/auth/auth.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 const routes = [...authRoutes];
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient()
+  ],
 };
