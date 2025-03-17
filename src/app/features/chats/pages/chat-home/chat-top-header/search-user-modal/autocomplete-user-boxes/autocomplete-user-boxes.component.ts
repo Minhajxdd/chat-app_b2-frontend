32d91dc161c340134f71339ce9cb1c317a26e88f
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-17 14:16:20 +0530

    feat(chat); autocomplete modal emitting done

--- src/app/features/chats/pages/chat-home/chat-top-header/search-user-modal/autocomplete-user-boxes/autocomplete-user-boxes.component.ts
+++ src/app/features/chats/pages/chat-home/chat-top-header/search-user-modal/autocomplete-user-boxes/autocomplete-user-boxes.component.ts
@@ -1,12 +1,20 @@
-import { Component, input } from '@angular/core';
+import { Component, ElementRef, input, output } from '@angular/core';
 import { User } from '../search-user.types';
 
 @Component({
   selector: 'app-autocomplete-user-boxes',
   imports: [],
   templateUrl: './autocomplete-user-boxes.component.html',
-  styleUrl: './autocomplete-user-boxes.component.css'
+  styleUrl: './autocomplete-user-boxes.component.css',
 })
 export class AutocompleteUserBoxesComponent {
+  constructor(private ele: ElementRef) {}
+
   user = input.required<User>();
+  onClick = output<User>();
+
+  onClickBox() {
+    this.onClick.emit(this.user());
+    this.ele.nativeElement.remove();
+  }
 }
