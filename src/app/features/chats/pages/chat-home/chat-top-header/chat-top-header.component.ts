1a4c44c234aacff785240baa67c3285cf35ea4df
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-17 18:07:04 +0530

    feat(chat); chat top three dot same toggle done

--- src/app/features/chats/pages/chat-home/chat-top-header/chat-top-header.component.ts
+++ src/app/features/chats/pages/chat-home/chat-top-header/chat-top-header.component.ts
@@ -1,16 +1,22 @@
 import { Component, signal } from '@angular/core';
 import { SearchUserModalComponent } from './search-user-modal/search-user-modal.component';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: '[app-chat-top-header]',
-  imports: [SearchUserModalComponent],
+  imports: [SearchUserModalComponent, NgClass],
   templateUrl: './chat-top-header.component.html',
   styleUrl: './chat-top-header.component.css',
 })
 export class ChatTopHeaderComponent {
   showSearchModal = signal(false);
+  showThreeDot = signal(false);
 
   onToggleSearchModal() {
     this.showSearchModal.set(!this.showSearchModal());
+  }
+
+  onToggleThreeDot() {
+    this.showThreeDot.set(!this.showThreeDot());
   }
 }
