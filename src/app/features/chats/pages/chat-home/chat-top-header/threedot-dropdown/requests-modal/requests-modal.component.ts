d0be43955b9d7f697c4ba7e7acd0973e6394df5f
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-17 21:26:35 +0530

    feat(chat): request-modal component added

---
+++ src/app/features/chats/pages/chat-home/chat-top-header/threedot-dropdown/requests-modal/requests-modal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-requests-modal',
+  imports: [],
+  templateUrl: './requests-modal.component.html',
+  styleUrl: './requests-modal.component.css'
+})
+export class RequestsModalComponent {
+
+}
