d5e3a8c6a7a1f2ce4bf264d2f1f88a07da7a091b
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-17 11:39:51 +0530

    feat(user): search-user-modal component added

---
+++ src/app/features/chats/pages/chat-home/chat-top-header/search-user-modal/search-user-modal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-user-modal',
+  imports: [],
+  templateUrl: './search-user-modal.component.html',
+  styleUrl: './search-user-modal.component.css'
+})
+export class SearchUserModalComponent {
+
+}
