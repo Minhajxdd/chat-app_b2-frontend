3d9723975990953b43ce4b05f85fd588aeaebc9e
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-17 11:43:52 +0530

    feat(chat); added basic modal things

--- src/app/features/chats/pages/chat-home/chat-top-header/search-user-modal/search-user-modal.component.ts
+++ src/app/features/chats/pages/chat-home/chat-top-header/search-user-modal/search-user-modal.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { CdkPortal, PortalModule } from '@angular/cdk/portal';
+import { Overlay, OverlayConfig, OverlayRef } from '@angular/cdk/overlay';
 
 @Component({
   selector: 'app-search-user-modal',
-  imports: [],
+  imports: [PortalModule],
   templateUrl: './search-user-modal.component.html',
-  styleUrl: './search-user-modal.component.css'
+  styleUrl: './search-user-modal.component.css',
 })
 export class SearchUserModalComponent {
+  constructor(private overlay: Overlay) {}
 
+  @ViewChild(CdkPortal) portal!: CdkPortal;
+
+  overlayRef!: OverlayRef;
+
+  openModal() {
+    const config = new OverlayConfig({
+      positionStrategy: this.overlay
+        .position()
+        .global()
+        .centerHorizontally()
+        .centerVertically(),
+      width: '60%',
+      hasBackdrop: true,
+    });
+
+    this.overlayRef = this.overlay.create(config);
+    this.overlayRef.attach(this.portal);
+    this.overlayRef.backdropClick().subscribe(() => this.overlayRef.detach());
+  }
+
+  closeModal() {
+    this.overlayRef.detach();
+  }
 }
