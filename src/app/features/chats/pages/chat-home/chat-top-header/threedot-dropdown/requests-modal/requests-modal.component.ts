d57e29cc464e0cf69e05bb9cef23cd39d967615d
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-17 21:48:35 +0530

    feat(chat): chat request ui done

--- src/app/features/chats/pages/chat-home/chat-top-header/threedot-dropdown/requests-modal/requests-modal.component.ts
+++ src/app/features/chats/pages/chat-home/chat-top-header/threedot-dropdown/requests-modal/requests-modal.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Overlay, OverlayConfig, OverlayRef } from '@angular/cdk/overlay';
+import { CdkPortal, PortalModule } from '@angular/cdk/portal';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-requests-modal',
-  imports: [],
+  imports: [PortalModule],
   templateUrl: './requests-modal.component.html',
   styleUrl: './requests-modal.component.css'
 })
 export class RequestsModalComponent {
 
+  constructor(private overlay: Overlay) {
+    setTimeout(() => {
+      this.openModal();
+    },50);
+  }
+
+  @ViewChild(CdkPortal) portal!: CdkPortal;
+
+  overlayRef!: OverlayRef;
+
+  openModal() {
+    const config = new OverlayConfig({
+      positionStrategy: this.overlay
+        .position()
+        .global()
+        .centerHorizontally()
+        .centerVertically(),
+      
+      hasBackdrop: true,
+    });
+
+    this.overlayRef = this.overlay.create(config);
+    this.overlayRef.attach(this.portal);
+    this.overlayRef.backdropClick().subscribe(() => this.overlayRef.detach());
+  }
+
+  closeModal() {
+    this.overlayRef.detach();
+  }
 }
--- src/app/features/chats/pages/chat-home/chat-top-header/threedot-dropdown/threedot-dropdown.component.ts
+++ src/app/features/chats/pages/chat-home/chat-top-header/threedot-dropdown/threedot-dropdown.component.ts
@@ -1,9 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { AuthService } from '../../../../../../core/auth/service/auth.service';
+import { RequestsModalComponent } from "./requests-modal/requests-modal.component";
 
 @Component({
   selector: 'app-threedot-dropdown',
-  imports: [],
+  imports: [RequestsModalComponent],
   templateUrl: './threedot-dropdown.component.html',
   styleUrl: './threedot-dropdown.component.css'
 })
