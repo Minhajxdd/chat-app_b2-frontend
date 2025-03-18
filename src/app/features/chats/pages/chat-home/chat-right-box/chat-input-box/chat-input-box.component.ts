5b97a14bf3d3ebe6deba682da4882c848849f16b
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-18 20:24:17 +0530

    feat(chat): chat input component

---
+++ src/app/features/chats/pages/chat-home/chat-right-box/chat-input-box/chat-input-box.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: '[app-chat-input-box]',
+  imports: [],
+  templateUrl: './chat-input-box.component.html',
+  styleUrl: './chat-input-box.component.css'
+})
+export class ChatInputBoxComponent {
+
+}
--- src/app/features/chats/pages/chat-home/chat-right-box/chat-right-box.component.ts
+++ src/app/features/chats/pages/chat-home/chat-right-box/chat-right-box.component.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { ChatSelectedConversationService } from '../../../services/chat-selected-conversation.service';
 import { Conversations } from '../chat-home.model';
+import { ChatSocketService } from '../../../services/chat-socket.service';
+import { ChatInputBoxComponent } from "./chat-input-box/chat-input-box.component";
 
 @Component({
   selector: '[app-chat-right-box]',
-  imports: [],
+  imports: [ChatInputBoxComponent],
   templateUrl: './chat-right-box.component.html',
   styleUrl: './chat-right-box.component.css',
 })
@@ -13,7 +15,8 @@ export class ChatRightBoxComponent {
   selectedConversation!: Conversations;
 
   constructor(
-    private readonly _chatSelectedConversationService: ChatSelectedConversationService
+    private readonly _chatSelectedConversationService: ChatSelectedConversationService,
+    private readonly _chatSocketService: ChatSocketService
   ) {
     this.subscribeToSelectedUsers();
   }
