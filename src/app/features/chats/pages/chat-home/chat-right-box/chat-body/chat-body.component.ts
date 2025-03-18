81f936323a609f8951b8ee0e69fb4123c20a3ae8
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-18 21:06:46 +0530

    feat(chat); message send boxy styled

--- src/app/features/chats/pages/chat-home/chat-right-box/chat-body/chat-body.component.ts
+++ src/app/features/chats/pages/chat-home/chat-right-box/chat-body/chat-body.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { MsgReceiveBoxComponent } from './msg-receive-box/msg-receive-box.component';
+import { MsgSentBoxComponent } from './msg-sent-box/msg-sent-box.component';
 
 @Component({
   selector: '[app-chat-body]',
-  imports: [MsgReceiveBoxComponent],
+  imports: [MsgReceiveBoxComponent, MsgSentBoxComponent],
   templateUrl: './chat-body.component.html',
   styleUrl: './chat-body.component.css'
 })
--- src/app/features/chats/pages/chat-home/chat-right-box/chat-body/msg-sent-box/msg-sent-box.component.ts
+++ src/app/features/chats/pages/chat-home/chat-right-box/chat-body/msg-sent-box/msg-sent-box.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-msg-sent-box',
+  selector: '[app-msg-sent-box]',
   imports: [],
   templateUrl: './msg-sent-box.component.html',
   styleUrl: './msg-sent-box.component.css'
