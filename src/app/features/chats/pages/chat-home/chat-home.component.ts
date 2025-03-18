785d3e28e33ce26a125522bb7daf7654de2f4ecc
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-18 16:34:42 +0530

    feat(chat): right box subscribed to selected

--- src/app/features/chats/pages/chat-home/chat-home.component.ts
+++ src/app/features/chats/pages/chat-home/chat-home.component.ts
@@ -1,9 +1,15 @@
-import { Component, DestroyRef, inject, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  DestroyRef,
+  inject,
+  ViewEncapsulation,
+} from '@angular/core';
 import { ChatSocketService } from '../../services/chat-socket.service';
-import { ChatRightBoxComponent } from "./chat-right-box/chat-right-box.component";
+import { ChatRightBoxComponent } from './chat-right-box/chat-right-box.component';
 import { ChatHomeService } from './chat-home.service';
 import { Conversations } from './chat-home.model';
 import { ChatTopHeaderComponent } from './chat-top-header/chat-top-header.component';
+import { ChatSelectedConversationService } from '../../services/chat-selected-conversation.service';
 
 @Component({
   selector: 'app-chat-home',
@@ -15,25 +21,35 @@ export class ChatHomeComponent {
   private readonly _chatSocketService = inject(ChatSocketService);
   private readonly _chatHomeService = inject(ChatHomeService);
   private readonly _destroyRef = inject(DestroyRef);
+  private readonly _chatSelectedConversationService = inject(
+    ChatSelectedConversationService
+  );
 
   conversations: Conversations[] = [];
 
   constructor() {
     this._chatSocketService.connect();
 
-    this._chatSocketService.emit('enter-chats', {});
+    // this._chatSocketService.emit('enter-chats', {});
 
-    const subscription = this._chatHomeService.getUsers()
-    .subscribe({
+    this.getUsers();
+  }
+
+  selectConversation(data: Conversations) {
+    this._chatSelectedConversationService.setConversation(data);
+  }
+
+
+  getUsers() {
+    const subscription = this._chatHomeService.getUsers().subscribe({
       next: (data) => {
         console.log(data);
         this.conversations = data.data;
-      }
+      },
     });
 
     this._destroyRef.onDestroy(() => {
       subscription.unsubscribe();
-    })
-
+    });
   }
 }
--- src/app/features/chats/pages/chat-home/chat-right-box/chat-right-box.component.ts
+++ src/app/features/chats/pages/chat-home/chat-right-box/chat-right-box.component.ts
@@ -1,4 +1,6 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { ChatSelectedConversationService } from '../../../services/chat-selected-conversation.service';
+import { Conversations } from '../chat-home.model';
 
 @Component({
   selector: '[app-chat-right-box]',
@@ -7,5 +9,26 @@ import { Component, inject } from '@angular/core';
   styleUrl: './chat-right-box.component.css',
 })
 export class ChatRightBoxComponent {
-  constructor() {}
+  isSelected = signal(false);
+  selectedConversation: Conversations | null = null;
+
+  constructor(
+    private readonly _chatSelectedConversationService: ChatSelectedConversationService
+  ) {
+    this.subscribeToSelectedUsers();
+  }
+
+  subscribeToSelectedUsers() {
+    const subscription = this._chatSelectedConversationService
+      .getConversationObservable()
+      .subscribe((data) => {
+        if (data) {
+          this.isSelected.set(true);
+          this.selectedConversation = data;
+        }
+
+        console.log(`This is the selected conversation`);
+        console.log(data);
+      });
+  }
 }
