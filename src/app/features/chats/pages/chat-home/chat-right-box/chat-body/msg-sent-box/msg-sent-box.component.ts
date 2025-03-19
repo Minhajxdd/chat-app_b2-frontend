2cde60ddd06051b7b227e7028ad1c17521e7cb7a
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-19 10:35:19 +0530

    feat(chat): chat send rendering part done

--- src/app/features/chats/pages/chat-home/chat-right-box/chat-body/msg-sent-box/msg-sent-box.component.ts
+++ src/app/features/chats/pages/chat-home/chat-right-box/chat-body/msg-sent-box/msg-sent-box.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { MessageDataModel } from '../chat-body.type';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: '[app-msg-sent-box]',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './msg-sent-box.component.html',
   styleUrl: './msg-sent-box.component.css'
 })
 export class MsgSentBoxComponent {
-
+  message = input.required<MessageDataModel>();
 }
