ddee5b7b511fce1c03914e5af508000a3cba045c
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-19 11:10:05 +0530

    feat(chat): chat send message part done

--- src/app/features/chats/pages/chat-home/chat-right-box/chat-body/chat-body.component.ts
+++ src/app/features/chats/pages/chat-home/chat-right-box/chat-body/chat-body.component.ts
@@ -4,6 +4,7 @@ import { MsgSentBoxComponent } from './msg-sent-box/msg-sent-box.component';
 import { ChatSocketService } from '../../../../services/chat-socket.service';
 import { UserState } from '../../../../../../shared/state/User/user.state';
 import { MessageDataModel } from './chat-body.type';
+import { ChatInputDataService } from '../chat-input-box/chat-input-data.service';
 
 @Component({
   selector: '[app-chat-body]',
@@ -20,18 +21,36 @@ export class ChatBodyComponent {
   constructor(
     private readonly _chatSocketService: ChatSocketService,
     private readonly _destoryRef: DestroyRef,
-    private readonly _userState: UserState
+    private readonly _userState: UserState,
+    private readonly _chatInputDataService: ChatInputDataService
   ) {
     this.subscribeToMessage();
+    this.subscribeToInputMessages();
+    this.getUserId();
   }
 
   subscribeToMessage() {
     const subscription = this._chatSocketService.on('message').subscribe({
       next: (data: { data: MessageDataModel }) => {
         this.receivedMessages.push(data.data);
-        console.log(data.data)
+        console.log(data.data);
       },
     });
+    this._destoryRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
+
+  subscribeToInputMessages() {
+    const subscription = this._chatInputDataService.message$.subscribe(
+      (data) => {
+        console.log('send data');
+        console.log(this.userId)
+        console.log(data);
+        this.receivedMessages.push(data);
+      }
+    );
+
     this._destoryRef.onDestroy(() => {
       subscription.unsubscribe();
     });
--- src/app/features/chats/pages/chat-home/chat-right-box/chat-input-box/chat-input-box.component.ts
+++ src/app/features/chats/pages/chat-home/chat-right-box/chat-input-box/chat-input-box.component.ts
@@ -4,6 +4,8 @@ import { FormsModule } from '@angular/forms';
 import { ChatSelectedConversationService } from '../../../../services/chat-selected-conversation.service';
 import { Conversations } from '../../chat-home.model';
 import { MessageBody } from './chat.type';
+import { ChatInputDataService } from './chat-input-data.service';
+import { UserState } from '../../../../../../shared/state/User/user.state';
 
 @Component({
   selector: '[app-chat-input-box]',
@@ -13,15 +15,19 @@ import { MessageBody } from './chat.type';
 })
 export class ChatInputBoxComponent {
   textInput: string = '';
+  userId: string = '';
 
   selectedConversation!: Conversations;
 
   constructor(
     private readonly _chatSocketService: ChatSocketService,
     private readonly _chatSelectedConversationService: ChatSelectedConversationService,
-    private readonly _destoryRef: DestroyRef
+    private readonly _destoryRef: DestroyRef,
+    private readonly _chatInputDataService: ChatInputDataService,
+    private readonly _userState: UserState
   ) {
     this.subscribeToSelectedUsers();
+    this.subscribeToUserState();
   }
 
   sendMessage() {
@@ -39,8 +45,25 @@ export class ChatInputBoxComponent {
         data.userId = this.selectedConversation.users[0]._id;
       }
 
+      this._chatInputDataService.sendMessage({
+        sender: this.userId,
+        conversation: data.conversationId,
+        text: text,
+        time: String(new Date()),
+      });
+
       this._chatSocketService.emit('message', data);
     }
+  }
+
+  subscribeToUserState() {
+    const subscription = this._userState.getData('_id').subscribe((data) => {
+      if (data) this.userId = data;
+    });
+
+    this._destoryRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
   }
 
   subscribeToSelectedUsers() {
