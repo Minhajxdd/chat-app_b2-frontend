389a4e2dc85c40d3c44546279c866d86851479fa
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-20 15:21:29 +0530

    feat(chat): chat event implmented

--- src/app/features/chats/pages/chat-home/chat-right-box/chat-event.service.ts
+++ src/app/features/chats/pages/chat-home/chat-right-box/chat-event.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ChatEventService {
+  private scrollToTopSubject = new Subject<void>();
 
-  constructor() { }
+  scrollToTop$ = this.scrollToTopSubject.asObservable();
+
+  emitScrollToTop() {
+    this.scrollToTopSubject.next();
+  }
 }
