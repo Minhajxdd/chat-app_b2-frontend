5eae2883c5a4d978c395f2078e4a292feae295d3
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-22 22:15:46 +0530

    feat(chat): chat form func done

--- src/app/features/chats/pages/chat-home/chat-top-header/threedot-dropdown/create-group-modal/create-group-modal.component.ts
+++ src/app/features/chats/pages/chat-home/chat-top-header/threedot-dropdown/create-group-modal/create-group-modal.component.ts
@@ -1,26 +1,52 @@
 import { Overlay, OverlayConfig, OverlayRef } from '@angular/cdk/overlay';
 import { CdkPortal, PortalModule } from '@angular/cdk/portal';
-import { Component, input, OnChanges, output, ViewChild } from '@angular/core';
+import {
+  Component,
+  input,
+  OnChanges,
+  OnInit,
+  output,
+  ViewChild,
+} from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-group-modal',
-  imports: [PortalModule],
+  imports: [PortalModule, ReactiveFormsModule],
   templateUrl: './create-group-modal.component.html',
   styleUrl: './create-group-modal.component.css',
 })
-export class CreateGroupModalComponent implements OnChanges {
+export class CreateGroupModalComponent implements OnChanges, OnInit {
   isModalOpen = input.required<boolean>();
   emitCloseModal = output();
+
+  groupForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.groupForm = this.fb.group({
+      groupTitle: ['', Validators.required],
+      groupDescription: ['', [Validators.required, Validators.minLength(10)]],
+    });
+  }
 
   ngOnChanges(): void {
     setTimeout(() => {
       if (this.isModalOpen() === true) {
         this.openModal();
       }
-    },500);
+    }, 500);
   }
 
-  constructor(private overlay: Overlay) {}
+  constructor(private overlay: Overlay, private fb: FormBuilder) {}
+
+  onSubmit(): void {
+    if (this.groupForm.valid) {
+      console.log('Form Submitted Successfully!', this.groupForm.value);
+    } else {
+      console.log('Form is not valid!');
+      this.groupForm.markAllAsTouched();
+    }
+  }
 
   // related to modal
   @ViewChild(CdkPortal) portal!: CdkPortal;
