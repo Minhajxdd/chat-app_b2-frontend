5651af40b667a16cbe7f29dcf863950ccc254ae8
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-22 22:26:10 +0530

    feat(chat): chat modal service implemented

--- src/app/features/chats/pages/chat-home/chat-top-header/threedot-dropdown/create-group-modal/create-group-modal.service.ts
+++ src/app/features/chats/pages/chat-home/chat-top-header/threedot-dropdown/create-group-modal/create-group-modal.service.ts
@@ -1,6 +1,27 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../../../../../../environments/environment';
+import { catchError, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class CreateGroupModalService {}
+export class CreateGroupModalService {
+  constructor(private readonly http: HttpClient) {}
+
+  createGroup(title: string, description: string) {
+    const payload = {
+      title,
+      description,
+    };
+    return this.http
+      .post(`${environment.back_end}/chat/group`, payload, {
+        withCredentials: true,
+      })
+      .pipe(
+        catchError((err: HttpErrorResponse) => {
+          return throwError(() => err.error.message);
+        })
+      );
+  }
+}
