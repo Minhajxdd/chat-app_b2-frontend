76e9670279ea78df8a8c0c1e7216f12673cbf2d2
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-23 16:06:26 +0530

    feat(chat): added user done

--- src/app/features/chats/pages/chat-home/chat-right-box/top-three-dot/add-user-modal/add-user-modal.component.ts
+++ src/app/features/chats/pages/chat-home/chat-right-box/top-three-dot/add-user-modal/add-user-modal.component.ts
@@ -1,11 +1,52 @@
-import { Component } from '@angular/core';
+import { Overlay, OverlayConfig, OverlayRef } from '@angular/cdk/overlay';
+import {  CdkPortal, PortalModule } from '@angular/cdk/portal';
+import { Component, input, output, ViewChild,} from '@angular/core';
 
 @Component({
   selector: 'app-add-user-modal',
-  imports: [],
+  imports: [PortalModule],
   templateUrl: './add-user-modal.component.html',
   styleUrl: './add-user-modal.component.css'
 })
 export class AddUserModalComponent {
+  isModalOpen = input.required<boolean>();
+  emitCloseModal = output();
 
+  ngOnChanges(): void {
+    setTimeout(() => {
+      if (this.isModalOpen() === true) {
+        this.openModal();
+      }
+      
+    }, 100);
+  }
+  
+  constructor(private overlay: Overlay) {}
+
+  @ViewChild(CdkPortal) portal!: CdkPortal;
+
+  overlayRef!: OverlayRef;
+
+  openModal() {
+    const config = new OverlayConfig({
+      positionStrategy: this.overlay
+        .position()
+        .global()
+        .centerHorizontally()
+        .centerVertically(),
+
+      hasBackdrop: true,
+    });
+
+    this.overlayRef = this.overlay.create(config);
+    this.overlayRef.attach(this.portal);
+    this.overlayRef.backdropClick().subscribe(() => this.closeModal());
+  }
+
+  closeModal() {
+    this.overlayRef.detach();
+    this.emitCloseModal.emit();
+  }
+
+  
 }
--- src/app/features/chats/pages/chat-home/chat-right-box/top-three-dot/top-three-dot.component.ts
+++ src/app/features/chats/pages/chat-home/chat-right-box/top-three-dot/top-three-dot.component.ts
@@ -1,11 +1,22 @@
-import { Component, input } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
+import { AddUserModalComponent } from "./add-user-modal/add-user-modal.component";
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-top-three-dot',
-  imports: [],
+  imports: [AddUserModalComponent, NgClass],
   templateUrl: './top-three-dot.component.html',
   styleUrl: './top-three-dot.component.css'
 })
 export class TopThreeDotComponent {
   chatType = input.required<string>();
+  isToggled = input.required<boolean>();
+
+  isAddUserToggled = signal(false);
+
+  toggleIsAddUserToggled() {
+    
+    this.isAddUserToggled.set(!this.isAddUserToggled());
+  }
+
 }
