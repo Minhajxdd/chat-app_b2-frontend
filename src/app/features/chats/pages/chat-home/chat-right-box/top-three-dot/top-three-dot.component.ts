e69e11b0adc5fb3f335b24fd0a5ea03a93f47e76
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-23 10:33:17 +0530

    feat(chat): selected chat three dot

--- src/app/features/chats/pages/chat-home/chat-right-box/top-three-dot/top-three-dot.component.ts
+++ src/app/features/chats/pages/chat-home/chat-right-box/top-three-dot/top-three-dot.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-top-three-dot',
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './top-three-dot.component.css'
 })
 export class TopThreeDotComponent {
-
+  chatType = input.required<string>();
 }
