dd043a1e76770c8258d6df9f051b445765fa91d6
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-23 10:05:57 +0530

    feat(chat): chat conversation listing implementions done

--- src/app/features/chats/pages/chat-home/chat-conversations-listings/chat-conversations-listsings-events.service.ts
+++ src/app/features/chats/pages/chat-home/chat-conversations-listings/chat-conversations-listsings-events.service.ts
@@ -1,6 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class ChatConversationsListingsEventsService {}
+export class ChatConversationsListingsEventsService {
+  private eventSubject = new Subject<any>();
+
+  event$ = this.eventSubject.asObservable();
+
+  emitEvent(data: any): void {
+    this.eventSubject.next(data);
+  }
+}
